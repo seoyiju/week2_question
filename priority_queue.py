20be9db675cb8ba04d5194104df6dd37e9b939b7
seoyiju <166982000+seoyiju@users.noreply.github.com>
2025-03-24 03:00:03 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -1,20 +1,22 @@
 class PriorityQueue:
   def __init__(self):
     self.heap = []
+    self.counter = 0  # tie-breaker
 
   def push(self, item, priority):
-    entry = (priority, item)
+    entry = (priority, self.counter, item)  # (우선순위, 순번, 아이템)
+    self.counter += 1
     self.heap.append(entry)
     self._sift_up(len(self.heap) - 1)
 
   def pop(self):
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
-      priority, item = self.heap.pop()
+      priority, count, item = self.heap.pop()
       self._sift_down(0)
       return item
     elif len(self.heap) == 1:
-      priority, item = self.heap.pop()
+      priority, count, item = self.heap.pop()
       return item
     else:
       return None
@@ -22,7 +24,7 @@ class PriorityQueue:
   def _sift_up(self, index):
     while index > 0:
       parent_index = (index - 1) // 2
-      if self.heap[parent_index][0] > self.heap[index][0]:
+      if self.heap[parent_index][:2] > self.heap[index][:2]:  # compare by (priority, counter)
         self._swap(parent_index, index)
         index = parent_index
       else:
@@ -35,11 +37,11 @@ class PriorityQueue:
       smallest = index
 
       if left_child_index < len(self.heap) and \
-        self.heap[left_child_index][0] < self.heap[smallest][0]:
+        self.heap[left_child_index][:2] < self.heap[smallest][:2]:
         smallest = left_child_index
 
       if right_child_index < len(self.heap) and \
-        self.heap[right_child_index][0] < self.heap[smallest][0]:
+        self.heap[right_child_index][:2] < self.heap[smallest][:2]:
         smallest = right_child_index
 
       if smallest != index:
